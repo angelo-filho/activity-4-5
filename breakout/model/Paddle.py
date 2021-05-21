bffeaacba8dc4780d7988f9ea4740426c39914bc
angelo-filho <amslf.snf20@uea.edu.br>
2021-05-21 18:03:56 -0400

    Add paddle collision with items

--- breakout/model/Paddle.py
+++ breakout/model/Paddle.py
@@ -1,9 +1,10 @@
 import pygame
 from breakout.control.constants import WIDTH, COLOR_PADDLE
+from breakout.model.Item import *
 
 
 class Paddle:
-    def __init__(self,x, y, width, height):
+    def __init__(self, x, y, width, height):
         self.rect = pygame.Rect(x, y, width, height)
 
         self.speed = 12
@@ -29,3 +30,43 @@ class Paddle:
 
     def render(self, screen):
         pygame.draw.rect(screen, COLOR_PADDLE, self.rect)
+
+
+class PaddleRemake(Paddle):
+    def __init__(self, x, y, width, height):
+        super().__init__(x, y, width, height)
+
+        self.huge_width = self.normal_width + 30
+
+        self.NORMAL_STATE = 0
+        self.HUGE_STATE = 1
+        self.current_state = self.NORMAL_STATE
+
+        self.frames_power_up = 0
+        self.MAX_FRAMES_POWER_UP = 60 * 6
+
+    def update(self):
+
+        if self.current_state == self.NORMAL_STATE:
+            self.normal_state_update()
+        else:
+            self.frames_power_up += 1
+
+            if self.frames_power_up > self.MAX_FRAMES_POWER_UP:
+                self.frames_power_up = 0
+                self.current_state = self.NORMAL_STATE
+
+            if self.current_state == self.HUGE_STATE:
+                self.huge_state_update()
+
+    def normal_state_update(self):
+        self.rect.width = self.normal_width
+
+    def huge_state_update(self):
+        print("Hi")
+        self.rect.width = self.huge_width
+
+    def collision_with_items(self, item):
+        self.frames_power_up = 0
+        if type(item) == GrowPaddleItem:
+            self.current_state = self.HUGE_STATE
