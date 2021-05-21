04c7bb12f38d36371092ffeb2c27cf402d8da2c6
angelo-filho <amslf.snf20@uea.edu.br>
2021-05-21 18:03:26 -0400

    Create items

---
+++ breakout/model/Item.py
@@ -0,0 +1,26 @@
+import pygame
+from breakout.control.constants import COLOR_BLACK
+from breakout.control.constants import HEIGHT
+
+
+class Item(pygame.sprite.Sprite):
+
+    def __init__(self, color, width, height):
+        super().__init__()
+        self.image = pygame.Surface([width, height])
+        self.image.fill(COLOR_BLACK)
+        self.image.set_colorkey(COLOR_BLACK)
+        pygame.draw.rect(self.image, color, [0, 0, width, height])
+        self.rect = self.image.get_rect()
+
+        self.GRAVITY = 3
+
+    def update(self):
+        self.rect.y += self.GRAVITY
+
+        if self.rect.y >= HEIGHT:
+            self.kill()
+
+
+class GrowPaddleItem(Item):
+    pass
