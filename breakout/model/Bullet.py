b40ec0d0dc823530a811a79a3d9fba24f4388359
angelo-filho <amslf.snf20@uea.edu.br>
2021-05-22 00:26:58 -0400

    Add bullets

---
+++ breakout/model/Bullet.py
@@ -0,0 +1,18 @@
+import pygame
+from breakout.control.constants import COLOR_BLACK
+
+
+class Bullet(pygame.sprite.Sprite):
+
+    def __init__(self, color, width, height):
+        super().__init__()
+        self.image = pygame.Surface([width, height])
+        self.image.fill(COLOR_BLACK)
+        self.image.set_colorkey(COLOR_BLACK)
+        pygame.draw.rect(self.image, color, [0, 0, width, height])
+        self.rect = self.image.get_rect()
+
+        self.speed = 4
+
+    def update(self):
+        self.rect.y -= self.speed
