c1c07d05af3782d566c2aa83fba305d994ca7335
Daniele Simas Guimaraes <dsg.snf20@uea.edu.br>
2021-05-22 22:24:54 -0400

    Remove: function pause

--- pong2/mypongpygame.py
+++ pong2/mypongpygame.py
@@ -6,13 +6,12 @@ from math import radians, cos, sin
 
 pygame.init()
 
+# Constants
 COLOR_BLACK = (0, 0, 0)
 COLOR_WHITE = (255, 255, 255)
 COLOR_GRAY_DARK = (134, 134, 134)
 COLOR_GRAY_DARKER = (104, 104, 104)
-
 SCORE_MAX = 2
-
 size = (1280, 720)
 screen = pygame.display.set_mode(size)
 pygame.display.set_caption("MyPong - PyGame Edition - 2021.01.30")
@@ -28,12 +27,8 @@ victory_font = pygame.font.Font('assets/PressStart2P.ttf', 100)
 victory_text_rect = score_text.get_rect()
 victory_text_rect.center = (420, 350)
 
-# Pause text
-pause_text = pygame.font.SysFont('Consolas', 32).render('Pause', True, pygame.color.Color('White'))
-
 
 def draw_text(text, font, color, surface, x, y):
-
     textobj = font.render(text, 1, color)
     textrect = textobj.get_rect()
     textrect.topleft = (x, y)
@@ -76,7 +71,6 @@ class Player1(Entity):
             self.rect.y -= self.speed
         else:
             self.rect.y += 0
-
         # player 1 down movement
         if self.move_down:
             self.rect.y += self.speed
@@ -87,7 +81,6 @@ class Player1(Entity):
         # player 1 collides with upper wall
         if self.rect.y <= 0:
             self.rect.y = 0
-
         # player 1 collides with lower wall
         elif self.rect.y >= 570:
             self.rect.y = 570
@@ -137,7 +130,6 @@ class Ball(Entity):
     def update(self):
         self.movement()
         self.collision_with_wall()
-
         # ball collision with the player 1 's paddle
         if self.rect.colliderect(player1.rect) and ball.dx < 0:
             self.collision_with_paddles()
@@ -205,18 +197,19 @@ game_state = "Menu"
 # Menu loop
 def main_menu():
     global game_state
-
     click = False
     while True:
         screen.fill((0, 0, 0))
-
         mx, my = pygame.mouse.get_pos()
 
+        # text draw
         draw_text("Pong", victory_font, (255, 255, 255), screen, screen.get_rect().centery + 80, 170)
 
+        # button
         button_1 = pygame.Rect(screen.get_rect().centery + 170, 370, 200, 60)
         button_2 = pygame.Rect(screen.get_rect().centery + 170, 470, 200, 60)
 
+        # color
         color1 = COLOR_GRAY_DARK
         color2 = COLOR_GRAY_DARK
 
@@ -234,8 +227,8 @@ def main_menu():
         pygame.draw.rect(screen, color2, button_2)
         draw_text("Play", score_font, (255, 255, 255), screen, screen.get_rect().centery + 185, button_1.y + 10)
         draw_text("Exit", score_font, (255, 255, 255), screen, screen.get_rect().centery + 185, button_2.y + 10)
-
         click = False
+
         for event in pygame.event.get():
             if event.type == QUIT:
                 pygame.quit()
@@ -247,11 +240,11 @@ def main_menu():
             if event.type == MOUSEBUTTONDOWN:
                 if event.button == 1:
                     click = True
-
         pygame.display.update()
         game_clock.tick(60)
 
 
+# Restart Game
 def restart_game():
     global score_1, score_2
     score_1 = 0
@@ -262,79 +255,64 @@ def restart_game():
 
 
 press_r_frames = 0
-RUNNING, PAUSE = 0, 1
-state = RUNNING 
+
 
 while game_loop:
 
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             game_loop = False
+        #  keystroke events
         if event.type == pygame.KEYDOWN:
-            if event.key == pygame.K_p: state = PAUSE
-            if event.key == pygame.K_s: state = RUNNING
+            if event.key == K_UP:
+                player1.move_up = True
+            if event.key == K_DOWN:
+                player1.move_down = True
+            if event.key == K_r and game_state == "Game" and (score_1 == SCORE_MAX or score_2 == SCORE_MAX):
+                restart_game()
+        if event.type == pygame.KEYUP:
+            if event.key == K_UP:
+                player1.move_up = False
+            if event.key == K_DOWN:
+                player1.move_down = False
+    if game_state == "Menu":
+        main_menu()
+    # checking the victory condition
+    if score_1 < SCORE_MAX and score_2 < SCORE_MAX and game_state == "Game":
+        # clear screen
+        screen.fill(COLOR_BLACK)
+        # scoring points
+        if ball.rect.x < -50:
+            ball.restart_ball()
+            score_2 += 1
+            scoring_sound_effect.play()
+        elif ball.rect.x > 1320:
+            ball.restart_ball()
+            score_1 += 1
+            scoring_sound_effect.play()
+        # update objects
+        score_text = score_font.render(str(score_1) + ' x ' + str(score_2), True, COLOR_WHITE, COLOR_BLACK)
+        player1.update()
+        player2.update()
+        ball.update()
+        # drawing objects
+        ball.render()
+        player1.render()
+        player2.render()
+        screen.blit(score_text, score_text_rect)
     else:
-        screen.fill((0, 0, 0))   
-        # keystroke events
-        if state == RUNNING:
-            #  keystroke events
-            if event.type == pygame.KEYDOWN:
-                if event.key == K_UP:
-                    player1.move_up = True
-                if event.key == K_DOWN:
-                    player1.move_down = True
-                if event.key == K_r and game_state == "Game" and (score_1 == SCORE_MAX or score_2 == SCORE_MAX):
-                    restart_game()
-
-            if event.type == pygame.KEYUP:
-                if event.key == K_UP:
-                    player1.move_up = False
-                if event.key == K_DOWN:
-                    player1.move_down = False
-
-        if game_state == "Menu":
-            main_menu()
-
-            # checking the victory condition
-        if score_1 < SCORE_MAX and score_2 < SCORE_MAX and game_state == "Game":
-            # clear screen
-            screen.fill(COLOR_BLACK)
-
-            # scoring points
-            if ball.rect.x < -50:
-                ball.restart_ball()
-                score_2 += 1
-                scoring_sound_effect.play()
-            elif ball.rect.x > 1320:
-                ball.restart_ball()
-                score_1 += 1
-                scoring_sound_effect.play()
-
-            # update objects
-            score_text = score_font.render(str(score_1) + ' x ' + str(score_2), True, COLOR_WHITE, COLOR_BLACK)
-            player1.update()
-            player2.update()
-            ball.update()
-
-            # drawing objects
-            ball.render()
-            player1.render()
-            player2.render()
-            screen.blit(score_text, score_text_rect)
+        # drawing victory
+        screen.fill(COLOR_BLACK)
+        press_r_frames += 1
+        if press_r_frames >= 30:
+            draw_text('PRESS R TO RESTART', score_font, COLOR_WHITE, screen, 260, 600)
+            if press_r_frames >= 60:
+                press_r_frames = 0
+        if score_1 == SCORE_MAX:
+            draw_text("VICTORY", victory_font, COLOR_WHITE, screen, victory_text_rect.x, victory_text_rect.y)
         else:
-            # drawing victory
-            screen.fill(COLOR_BLACK)
-            press_r_frames += 1
-            if press_r_frames >= 30:
-                draw_text('PRESS R TO RESTART', score_font, COLOR_WHITE, screen, 260, 600)
-                if press_r_frames >= 60:
-                    press_r_frames = 0
-
-            if score_1 == SCORE_MAX:
-                draw_text("VICTORY", victory_font, COLOR_WHITE, screen, victory_text_rect.x, victory_text_rect.y)
-            else:
-                draw_text("DEFEAT", victory_font, COLOR_WHITE, screen, 360, victory_text_rect.y)
-            screen.blit(score_text, score_text_rect)                   
+            draw_text("DEFEAT", victory_font, COLOR_WHITE, screen, 360, victory_text_rect.y)
+        screen.blit(score_text, score_text_rect)
 
     # update screen
     pygame.display.flip()
